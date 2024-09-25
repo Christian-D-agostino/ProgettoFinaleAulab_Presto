556093ecc6f0b820e8ed24525d461f5ff41faa8f
Mario Fiorelli <mario.fiorelli92@gmail.com>
2024-09-25 17:27:59 +0200

    US9_completa

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -3,7 +3,9 @@
 namespace App\Jobs;
 
 use Spatie\Image\Image;
+use Spatie\Image\Enums\Unit;
 use Spatie\Image\Enums\CropPosition;
+use Spatie\Image\Enums\AlignPosition;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Foundation\Queue\Queueable;
@@ -39,6 +41,15 @@ class ResizeImage implements ShouldQueue
 
         Image::load($srcPath)
              ->crop($w, $h, CropPosition::Center)
+             ->watermark(
+                base_path('public/media/watermark2.png'),
+                width: 500,
+                height: 500,
+                paddingX: 0,
+                paddingY:0,    
+                // paddingUnit: Unit::Percent,
+               
+                )
              ->save($destPath);
     }
 }
