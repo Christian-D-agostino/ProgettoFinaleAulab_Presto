e480439838d1ba4163fdb3179b5aae846834e53e
Mario Fiorelli <mario.fiorelli92@gmail.com>
2024-10-02 12:27:36 +0200

    fine

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,13 +1,13 @@
 <x-layout>
 
-    <div class="container-fluid pt-5 height-custom">
+    <div class="container-fluid  height-custom">
         
-        <div class="row  justify-content-center align-items-center text-center pt-5">
+        <div class="row  justify-content-center align-items-center text-center ">
             <div class="col-12">
-                <h1 class="display-4 pt-5">{{__('ui.allarticles')}}</h1>
+                <h1 class="display-4 ">{{__('ui.allarticles')}}</h1>
             </div>
         </div>
-        <div class="row  justify-content-center align-items-center py-4 gap-3">
+        <div class="row  justify-content-center align-items-center gap-3">
            
                 @forelse ($articles as $article)
                     <div class="col-12 col-md-3 pt-5 d-flex justify-content-center">
@@ -15,7 +15,7 @@
                     </div>
                 @empty
                     <div class="col-12">
-                        <h3 class="text-center">{{__('ui.noitemsloaded')}} </h3>
+                        <h3 class="text-center ">{{__('ui.noitemsloaded')}} </h3>
                     </div>
                 @endforelse
         
